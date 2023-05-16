d926a5d580f6edd23b4c2abecbfa855376528885
Jula <loveliljan876@wp.pl>
2023-05-16 19:21:58 +0200

    Dodaj pliki projektów.

---
+++ Pages/History.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PS6.Areas.Data.Models;
+using PS6.Areas.YearDb;
+
+namespace PS6.Pages.History
+{
+    public class IndexModel : PageModel
+    {
+        private readonly PS6.Areas.YearDb.YearDbContext _context;
+
+        public IndexModel(PS6.Areas.YearDb.YearDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<YearValidationResult> YearValidationResult { get;set; } = default!;
+
+        public async Task OnGetAsync(int pageIndex = 1)
+        {
+            if (_context.YearValidationResult != null)
+            {
+                YearValidationResult = await PaginatedList<YearValidationResult>.CreateAsync(
+                _context.YearValidationResult.AsNoTracking(), pageIndex, 20);
+            }
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using PS6.Areas.Data.Models;
+using PS6.Areas.YearDb;
+using PS6.Pages.Forms;
+using System.Security.Claims;
+
+namespace PS6.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly YearDbContext dbContext;
+
+        [BindProperty(SupportsGet = true)]
+        public YearForm Form { get; set; }
+
+        public YearResponse? YearResponse { get; set; }
+
+        public IndexModel(YearDbContext dbContext)
+        {
+            Form = new YearForm();
+            YearResponse = null;
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                YearResponse = new YearResponse(Form.Name, Form.Year.Value);
+                SaveInSession(YearResponse);
+                SaveInDatabase(YearResponse);
+            }
+
+            return Page();
+        }
+
+        public List<YearResponse> GetResponses()
+        {
+            List<YearResponse>? currentList = new List<YearResponse>();
+            var data = HttpContext.Session.GetString("Data");
+
+            if (data != null)
+            {
+                currentList = JsonConvert.DeserializeObject<List<YearResponse>>(data);
+                if (currentList == null)
+                {
+                    currentList = new List<YearResponse>();
+                }
+            }
+
+            return currentList;
+        }
+
+        public void SaveInSession(YearResponse response)
+        {
+            var currentList = GetResponses();
+
+            currentList.Add(response);
+            HttpContext.Session.SetString("Data", JsonConvert.SerializeObject(currentList));
+        }
+
+        public void SaveInDatabase(YearResponse response)
+        {
+            var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var userName = HttpContext.User.Identity.Name;
+
+            var yearValidationResult = new YearValidationResult
+            {
+                Year = response.Year,
+                Result = response.ToString(),
+                TimeAdded = DateTime.Now,
+                UserId = userId,
+                UserLogin = userName
+            };
+
+            dbContext.YearValidationResult.Add(yearValidationResult);
+            dbContext.SaveChanges();
+        }
+    }
+}
